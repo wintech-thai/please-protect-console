7dd36dabb36f5e5eb121842c737c8c325c0fd007
auttz <auttzmargaretz@gmail.com>
2026-04-27 13:27:14 +0700

    Change title bar to Please Protect Sensor

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,7 +2,7 @@ import SignInView from "@/modules/auth/view/sign-in.view";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "PLEASE-PROTECT-SENSOR",
+  title: "PLEASE-PROTECT-SENSOR", // etc. "PLEASE-PROTECT-SENSOR - Login"
   description: "Please-Protect Sensor Web Interface",
 };
 
